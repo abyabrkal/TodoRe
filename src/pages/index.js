627d9652aaf8fdd7e72ba45c25a18e911d1a3a04
Aby Abraham <abyabrkal@gmail.com>
2018-09-05 16:26:40 +0400

    form skeleton added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
 import ReactDOM from 'react-dom'
 import './index.css'
+import TodoList from './TodoList'
 
-export default () => <div>Hello world!</div>
+//export default () => <div>Hello world!</div>
+//var destination = document.querySelector('#container');
+
+
+const IndexPage = () => (
+    <div>
+        <TodoList />
+    </div>
+)
+
+export default IndexPage;
